e2f4ad2b3b3d1d18d3f342b2a5013fb77f8a6f17
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2017-11-27 10:40:31 -0800

    reverting back changes

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ mongoose.connect(
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/public"));
+    app.use(express.static("client/build"));
   }
   
   // Send every request to the React app
   // Define any API routes before this runs
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 // Start the API server
 // after latest build
